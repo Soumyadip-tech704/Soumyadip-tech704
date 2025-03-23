efa362115b096f38b0b255d9928b9b5192c22e54
Soumyadip-tech704 <sahasoumyadip95@gmail.com>
2025-03-24 01:18:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+class paper{
+
+    init(paper){
+
+    paper.addEventListener('mousedown', (e) => {
+        alert('mouse is selected the paper element');
+    })
+}
+
+const papers=Array.from(document.querySelectorAll('.paper'));
+
+papers.forEach( paper=>{
+    const p = new paper();
+    p.init();
+})
